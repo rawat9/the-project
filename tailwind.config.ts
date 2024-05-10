cef7f62b35c43563138e9e35b95602b1495e5649
Anurag Rawat <anurag.rawat0201@gmail.com>
2024-05-10 14:22:03 +0100

    remove default tailwind styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,13 +6,7 @@ const config: Config = {
     "./src/app/**/*.{ts,tsx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
