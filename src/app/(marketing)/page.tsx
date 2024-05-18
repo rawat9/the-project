591ed0aaf8138492640d0ef8c97365ec40d7564e
Anurag Rawat <anurag.rawat0201@gmail.com>
2024-05-18 12:07:38 +0100

    remove @shadcn/ui button; move navbar to /components

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,38 +1,5 @@
-'use client'
-
-import {
-  Navbar,
-  NavbarBrand,
-  NavbarContent,
-  NavbarItem,
-  Link,
-  Button,
-} from '@nextui-org/react'
+import { Navbar } from '~/components/navbar'
 
 export default function Page() {
-  return (
-    <Navbar>
-      <NavbarBrand>
-        {/* <AcmeLogo /> */}
-        <p className="font-bold text-inherit">Expose</p>
-      </NavbarBrand>
-      <NavbarContent className="hidden gap-4 sm:flex" justify="start">
-        <NavbarItem>
-          <Link color="foreground" href="#">
-            Explore
-          </Link>
-        </NavbarItem>
-      </NavbarContent>
-      <NavbarContent justify="end">
-        <NavbarItem className="hidden lg:flex">
-          <Link href="/sign-in">Login</Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Button as={Link} color="primary" href="/sign-up" variant="flat">
-            Sign Up
-          </Button>
-        </NavbarItem>
-      </NavbarContent>
-    </Navbar>
-  )
+  return <Navbar />
 }
---
+++ src/components/navbar.tsx
@@ -0,0 +1,37 @@
+'use client'
+
+import {
+  Navbar as NextUiNavbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Link,
+  Button,
+} from '@nextui-org/react'
+
+export function Navbar() {
+  return (
+    <NextUiNavbar>
+      <NavbarBrand>
+        <p className="font-bold text-inherit">Expose</p>
+      </NavbarBrand>
+      <NavbarContent className="hidden gap-4 sm:flex" justify="start">
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Explore
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <NavbarItem className="hidden lg:flex">
+          <Link href="/sign-in">Login</Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Button as={Link} color="primary" href="/sign-up" variant="flat">
+            Sign Up
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+    </NextUiNavbar>
+  )
+}
