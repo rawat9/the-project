903f62d79ee571c2f36c6038de30ab4895fda395
ananyachibber21 <ananyaunique21@gmail.com>
2024-05-19 21:12:56 +0530

    modified hero-section

--- src/app/(marketing)/_components/hero-section.tsx
+++ src/app/(marketing)/_components/hero-section.tsx
@@ -25,12 +25,10 @@ export default function Hero() {
         </div>
         <div className="text-center">
           <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-            Data to enrich your online business
+            Build Your Impressive Portfolio in Minutes!
           </h1>
           <p className="mt-6 text-lg leading-8 text-gray-600">
-            Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-            lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
-            fugiat aliqua.
+          Ready to unleash your creativity and showcase your talents to the world? Sign up now and start creating your impressive portfolio with Expose today!
           </p>
           <div className="mt-10 flex items-center justify-center gap-x-6">
             <a
