692abff3d7589f73711e65fe82f62d1ad6ef0028
ananyachibber21 <ananyaunique21@gmail.com>
2024-05-19 21:11:52 +0530

    applied transparent background

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -11,20 +11,20 @@ import {
 
 export function Navbar() {
   return (
-    <NextUiNavbar>
+    <NextUiNavbar className="bg-transparent">
       <NavbarBrand>
         <p className="font-bold text-inherit">Expose</p>
       </NavbarBrand>
-      <NavbarContent className="hidden gap-4 sm:flex" justify="start">
+      {/* <NavbarContent className="hidden gap-4 sm:flex" justify="start">
         <NavbarItem>
           <Link color="foreground" href="#">
             Explore
           </Link>
         </NavbarItem>
-      </NavbarContent>
+      </NavbarContent> */}
       <NavbarContent justify="end">
         <NavbarItem className="hidden lg:flex">
-          <Link href="/sign-in">Login</Link>
+          <Link href="/sign-in" size='sm'>Login</Link>
         </NavbarItem>
         <NavbarItem>
           <Button as={Link} color="primary" href="/sign-up" variant="flat">
