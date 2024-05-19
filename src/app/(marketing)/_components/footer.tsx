8141791cbc8b6fde6101b878be154334f0311be0
ananyachibber21 <ananyaunique21@gmail.com>
2024-05-19 14:47:51 +0530

    Changes in footer section

--- src/app/(marketing)/_components/footer.tsx
+++ src/app/(marketing)/_components/footer.tsx
@@ -8,8 +8,6 @@ export function PageFooter() {
       <Footer.Copyright href="#" by="Flowbite™" year={2022} />
       <Footer.LinkGroup>
         <Footer.Link href="#">About</Footer.Link>
-        <Footer.Link href="#">Privacy Policy</Footer.Link>
-        <Footer.Link href="#">Licensing</Footer.Link>
         <Footer.Link href="#">Contact</Footer.Link>
       </Footer.LinkGroup>
     </Footer>
