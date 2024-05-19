cd1e1d40ffd77cb3d600726b5b87716c1b87177d
Anurag Rawat <anurag.rawat0201@gmail.com>
2024-05-19 09:38:25 +0100

    add auth routes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import '../styles/globals.css'
 
+// Providers
+import { ClerkProvider } from '@clerk/nextjs'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -17,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className} suppressHydrationWarning={true}>
-        {children}
+        <ClerkProvider>{children}</ClerkProvider>
       </body>
     </html>
   )
