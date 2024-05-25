6fa075c1c93e13b845edd91d3418c1972dbe3d38
Anurag Rawat <anurag.rawat0201@gmail.com>
2024-05-25 07:37:59 +0100

    add metadata

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,4 +1,9 @@
 import { SignIn } from '@clerk/nextjs'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Expose | Sign In',
+}
 
 export default function Page() {
   return <SignIn path="/sign-in" />
--- src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,4 +1,9 @@
 import { SignUp } from '@clerk/nextjs'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Expose | Sign Up',
+}
 
 export default function Page() {
   return <SignUp path="/sign-up" />
